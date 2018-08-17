9ceaab9e1606ab531e05c044f63ed74a03675c98
Jacob Liberman <jliberma@redhat.com>
2018-08-17 17:21:48 -0400

    add new env files

--- scripts/overcloud-deploy.sh
+++ scripts/overcloud-deploy.sh
@@ -7,10 +7,13 @@ exec openstack overcloud deploy \
 -r /home/stack/templates/roles_data.yaml \
 -e /home/stack/templates/node-count.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/octavia.yaml \
 -e /home/stack/templates/environments/10-ntp.yaml \
 -e /home/stack/templates/environments/10-network.yaml \
 -e /home/stack/templates/environments/20-network-environment.yaml \
+-e /home/stack/templates/environments/20-compute-params.yml \
+-e /home/stack/templates/environments/20-controller-params.yml \
 -e /home/stack/templates/environments/25-hostname-map.yaml \
 -e /home/stack/templates/environments/30-ips-from-pool-all.yaml \
 -e /home/stack/templates/environments/50-vip.yaml \
